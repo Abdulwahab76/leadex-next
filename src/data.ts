f322cc20fe7db4d15b62116fb65b670f1f3a84c0
Abdulwahab76 <abdulwahab12.pk@gmail.com>
2026-01-17 01:09:43 +0500

    feat: imags & data

---
+++ src/data.ts
@@ -0,0 +1,26 @@
+export const roofFlashingProducts = [
+    {
+        slug: "wakaflex",
+        name: "TRUSTED ON AUSSIE HOMES SINCE 2001",
+        image: "/images/wakaflex-roll.png",
+        description: "Flexible flashing solution for roofing applications.",
+    },
+    {
+        slug: "flexoflash",
+        name: "Flexoflash",
+        image: "/images/flexoflash-packaging.png",
+        description: "High performance roof flashing membrane.",
+    },
+    {
+        slug: "dakaflash",
+        name: "Dakaflash",
+        image: "/images/dakaflash-roll.png",
+        description: "Self-adhesive flashing for roof details.",
+    },
+    {
+        slug: "smartform",
+        name: "Smartform",
+        image: "/images/smartform-roll.png",
+        description: "Mouldable flashing system for complex shapes.",
+    },
+];
